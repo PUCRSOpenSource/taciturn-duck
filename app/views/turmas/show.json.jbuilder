ef15bb41959c501d598f951b5fa4d1e3c22cdf56
Matthias Nunes <matthiasnunes@gmail.com>
2015-10-25 19:43:25 -0200

    adds turma

---
+++ app/views/turmas/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @turma, :id, :horario, :disciplina_id, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :turmas
   resources :pre_requisitos
   resources :disciplinas
   resources :cursos

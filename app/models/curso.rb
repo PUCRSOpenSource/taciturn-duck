fd7a8569d89cbf0067b65294eb090cb3cf2a2934
Matthias Nunes <matthiasnunes@gmail.com>
2015-10-31 21:57:40 -0200

    fixes model relations

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,2 +1,4 @@
 class Curso < ActiveRecord::Base
+	has_many :disciplinas, through: :curriculo
+	has_many :curriculos
 end
--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,2 +1,4 @@
 class Disciplina < ActiveRecord::Base
+	has_many :cursos, through: :curriculo
+	has_many :curriculos
 end

43354b0f8fdffa88123c953a7ba47e8bf3a31839
Diego Pinto da Jornada <diego.p.jornada@gmail.com>
2015-10-31 22:21:03 -0200

    Updates seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -111,5 +111,6 @@ disciplina_list = [
 ]
 
 disciplina_list.each do |disciplina|
-	Disciplina.create(nome: disciplina[0], creditos:disciplina[1], semestre: disciplina[2])
+	disc = Disciplina.create(nome: disciplina[0], creditos:disciplina[1], semestre: disciplina[2])
+	Curriculo.create(codigo: '4406', curso_id: 7, disciplina_id: disc.id)
 end

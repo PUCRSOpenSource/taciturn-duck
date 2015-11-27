b7ffd2e8f728e55c16bd1a1effa050947c792713
Matthias Nunes <matthiasnunes@gmail.com>
2015-11-27 18:41:49 -0200

    adds tests for curso

--- test/models/curso_test.rb
+++ test/models/curso_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class CursoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	
+	test "should not save without nome" do
+		curso = Curso.new grau: "Bacharelado"
+		assert_not curso.save
+		curso.nome = "Direito"
+		assert curso.save
+	end
+
+	test "should not save without grau" do
+		curso = Curso.new nome: "Direito"
+		assert_not curso.save
+		curso.grau = "Bacharelado"
+		assert curso.save
+	end
+
 end

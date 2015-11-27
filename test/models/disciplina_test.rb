7312f71f5a8469cc1b17d81f3ca7c4ac2002cf7e
Matthias Nunes <matthiasnunes@gmail.com>
2015-11-27 18:25:03 -0200

    adds tests for disciplina

--- test/models/disciplina_test.rb
+++ test/models/disciplina_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class DisciplinaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "should not save disciplina without nome" do
+		disciplina = Disciplina.new creditos: 6, semestre: 3
+		assert_not disciplina.save
+		disciplina.nome = "Algoritmos e Programação III"
+		assert disciplina.save
+	end
+
+	test "should not save disciplina without creditos" do
+		disciplina = Disciplina.new nome: "Algoritmos e Programação III", semestre: 3
+		assert_not disciplina.save
+		disciplina.creditos = 6
+		assert disciplina.save
+	end
+
+	test "should not save disciplina without semestre" do
+		disciplina = Disciplina.new nome: "Algoritmos e Programação III", creditos: 6
+		assert_not disciplina.save
+		disciplina.semestre = 3
+		assert disciplina.save
+	end
+
 end

70f21c12a18e680021fb9815f4aa582737feda96
Matthias Nunes <matthiasnunes@gmail.com>
2015-11-28 04:09:31 -0200

    adds tests for curriculo

--- test/models/curriculo_test.rb
+++ test/models/curriculo_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CurriculoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	test "should not save without codigo" do
+		cur = Curriculo.new curso_id: 7, disciplina_id: 1
+		assert_not cur.save
+		cur.codigo = "4406"
+		assert cur.save
+	end
+
+	test "should not save without curso_id" do
+		cur = Curriculo.new codigo: "4406", disciplina_id: 1
+		assert_not cur.save
+		cur.curso_id = 7
+		assert cur.save
+	end
+
+	test "should not save without disciplina_id" do
+		cur = Curriculo.new codigo: "4406", curso_id: 7
+		assert_not cur.save
+		cur.disciplina_id = 1
+		assert cur.save
+	end
+
 end

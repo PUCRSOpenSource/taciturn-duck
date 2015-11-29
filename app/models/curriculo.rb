0c1973e5003f01a9e84866cbf4fc7cee44fb6df3
Matthias Nunes <matthiasnunes@gmail.com>
2015-11-29 15:55:13 -0200

    all curriculos tests pass

--- app/models/curriculo.rb
+++ app/models/curriculo.rb
@@ -2,4 +2,8 @@ class Curriculo < ActiveRecord::Base
 	belongs_to :curso
 	has_many :disciplinas
 	accepts_nested_attributes_for :disciplinas, reject_if: :all_blank, allow_destroy: true
+
+	validates :codigo, presence: true
+	validates :curso_id, presence: true
+	validates :disciplina_id, presence: true
 end

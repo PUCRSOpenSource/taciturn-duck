b4a80c6ea778b0ff12703fe9200588d50f30f976
Matthias Nunes <matthiasnunes@gmail.com>
2015-11-29 16:00:37 -0200

    pass all tests for disciplina

--- app/models/disciplina.rb
+++ app/models/disciplina.rb
@@ -1,4 +1,8 @@
 class Disciplina < ActiveRecord::Base
 	has_many :cursos, through: :curriculo
 	has_many :curriculos
+
+	validates :nome, presence: true
+	validates :creditos, presence: true
+	validates :semestre, presence: true
 end

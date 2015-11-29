0565cc2a7701a5cb53107adc6d51cf67600ca4ae
Matthias Nunes <matthiasnunes@gmail.com>
2015-11-29 16:20:20 -0200

    pass all tests for turma

--- app/models/turma.rb
+++ app/models/turma.rb
@@ -1,3 +1,7 @@
 class Turma < ActiveRecord::Base
   belongs_to :disciplina
+
+	validates :horario, presence: true, format: { with: /\A([0-9][A-Z]{2,4}){1,3}\z/ }
+	validates :numero, presence: true, numericality: { only_integer: true }
+	validates :disciplina_id, presence: true, numericality: { only_integer: true }
 end
